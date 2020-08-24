45f22183e4ce17d960d8c54af162fd20e6d82921
foisal0337 <59086451+foisal0337@users.noreply.github.com>
2020-08-24 13:02:07 +0600

    Add files via upload

---
+++ number_theory/uva543.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define n 1000000
+
+bool prime[n+1];
+void seiv()
+{
+    memset(prime,true,sizeof(prime));
+
+    for(int i=2;i*i<=n;i++){
+        if(prime[i]==true){
+            for(int j=i*i;j<=n;j=j+i){
+                prime[j]=false;
+            }
+        }
+    }
+    prime[2]=false;
+}
+
+int main()
+{
+    seiv();
+    int a;
+    while(cin>>a && a )
+    {
+        for(int i=3;i<=a;i++){
+            if(prime[i]==true){
+                int b = a-i;
+                if(prime[b]==true){
+                    cout << a << " = " << i << " + " << b << endl;
+                    break;
+                }
+            }
+        }
+
+    }
+}
